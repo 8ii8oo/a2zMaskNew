15d4a88d15c4398fa9ced093bbd2ed5209d4c52d
8ii8oo <gungchi06@gmail.com>
2025-11-10 02:31:36 +0900

    포탈 최종 수정

--- Assets/script/PotalScene.cs
+++ Assets/script/PotalScene.cs
@@ -14,17 +14,26 @@ public class PotalScene : MonoBehaviour
     private string speedPropName = "_speed";
     private string scalePropName = "_scale";
 
- 
-    //obj.GetComponent<Renderer>().material.SetFloat("변수이름", 값);
-    
+
     void Start()
     {
-        if(filterRD != null)
+        DontDestroyOnLoad(gameObject);
+
+        SceneManager.sceneLoaded += OnSceneLoad;
+
+        if (filterRD != null)
         {
             filterRD.material.SetFloat(speedPropName, 0f);
             filterRD.material.SetFloat(scalePropName, 0f);
         }
-        
+
+
+
+    }
+
+    void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoad;
     }
 
 
@@ -36,7 +45,7 @@ public class PotalScene : MonoBehaviour
             StartCoroutine(LoadSceneDelay(other.gameObject));
         }
     }
-    
+
     IEnumerator LoadSceneDelay(GameObject PlayerToStop)
     {
 
@@ -54,6 +63,9 @@ public class PotalScene : MonoBehaviour
         {
             playerMoveScript.enabled = false;
         }
+
+        yield return new WaitForSeconds(1f);
+
         Debug.Log("점점커짐");
         float duration = 3f;
         float elapsedTime = 0f;
@@ -63,6 +75,52 @@ public class PotalScene : MonoBehaviour
         float startScale = 0f;
         float targetScale = 50f;
 
+
+        while (elapsedTime < duration)
+        {
+            float t = elapsedTime / duration;
+            float currentSpeed = Mathf.Lerp(startSpeed, targetSpeed, t);
+            float currentScale = Mathf.Lerp(startScale, targetScale, t);
+
+                if (filterRD != null)
+                {
+                    filterRD.material.SetFloat(speedPropName, currentSpeed);
+                    filterRD.material.SetFloat(scalePropName, currentScale);
+                }
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+
+        }
+
+        if (filterRD != null)
+        {
+            filterRD.material.SetFloat(speedPropName, targetSpeed);
+            filterRD.material.SetFloat(scalePropName, targetScale);
+        }
+
+        SceneManager.LoadScene(sceneName);
+    }
+
+    void OnSceneLoad(Scene scene, LoadSceneMode mode)
+    {
+        StartCoroutine(RestoreEffect());
+    }
+    
+    IEnumerator RestoreEffect()
+    {
+
+        
+        if (filterRD == null) yield break;
+        filterRD.gameObject.SetActive(true);
+
+        float duration = 3f;
+        float elapsedTime = 0f;
+
+        float startSpeed = 3f;
+        float targetSpeed = 0f;
+        float startScale = 50f;
+        float targetScale = 0f;
 
         while (elapsedTime < duration)
         {
@@ -80,13 +138,10 @@ public class PotalScene : MonoBehaviour
             yield return null;
 
         }
-
-        if (filterRD != null)
-            {
+                
                 filterRD.material.SetFloat(speedPropName, targetSpeed);
                 filterRD.material.SetFloat(scalePropName, targetScale);
-            }
-
-        SceneManager.LoadScene(sceneName);
+        
+        }
     }
-}
+
