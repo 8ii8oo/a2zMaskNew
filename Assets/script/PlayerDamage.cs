b3496180c4dcb108395995d122b4197a116e1bfe
8ii8oo <gungchi06@gmail.com>
2025-12-07 13:43:33 +0900

    블랙 단일 공격

--- Assets/script/PlayerDamage.cs
+++ Assets/script/PlayerDamage.cs
@@ -6,15 +6,18 @@ public class PlayerDamage : MonoBehaviour
 {
     public float damageAmount = 10f;
 
-    // 이번 공격 동안 이미 맞은 적들 저장
-    private HashSet<EnemyHp> hitEnemies = new HashSet<EnemyHp>();
+    private HashSet<EnemyHp> hitEnemies = new HashSet<EnemyHp>(); 
 
-    public bool isBlackSkill = false;
+    public bool isBlackSkill = false; 
+    
+
+    private bool hasHitTarget = false; 
 
     void OnEnable()
     {
-        hitEnemies.Clear();   // 공격이 새로 시작되면 리셋
+        hitEnemies.Clear(); 
         isBlackSkill = false;
+        hasHitTarget = false;
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -25,19 +28,25 @@ public class PlayerDamage : MonoBehaviour
         if (enemy == null) enemy = other.GetComponentInParent<EnemyHp>();
         if (enemy == null) return;
 
-        if (hitEnemies.Contains(enemy)) return;
-
-        hitEnemies.Add(enemy);
-        enemy.TakeDamage(damageAmount);
-
-        if(isBlackSkill)
+        if (isBlackSkill)
         {
-           gameObject.SetActive(false);
+            if (hasHitTarget) 
+            {
+                return; 
+            }
+
+            hasHitTarget = true;
+            gameObject.SetActive(false); 
         }
+        else // 일반 공격
+        {
+            if (hitEnemies.Contains(enemy)) return;
+            hitEnemies.Add(enemy);
+        }
+
+        enemy.TakeDamage(damageAmount);
     }
-
     
-
     public void SetDamage(float dmg)
     {
         damageAmount = dmg;
