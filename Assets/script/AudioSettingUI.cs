eef143b6b57c5a0580b4ae2509b2d8cc6f03fe98
8ii8oo <gungchi06@gmail.com>
2025-12-18 22:45:54 +0900

    사운드 숫자 입력

--- Assets/script/AudioSettingUI.cs
+++ Assets/script/AudioSettingUI.cs
@@ -1,30 +1,75 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class AudioSettingUI : MonoBehaviour
 {
+    [Header("BGM")]
     public Slider bgmSlider;
+    public TMP_InputField bgmInput;
+
+    [Header("SFX")]
     public Slider sfxSlider;
+    public TMP_InputField sfxInput;
 
     void Start()
     {
         if (AudioManager.instance != null)
         {
+            // 초기값
             bgmSlider.value = AudioManager.instance.bgmVolume;
             sfxSlider.value = AudioManager.instance.sfxVolume;
+
+            bgmInput.text = Mathf.RoundToInt(AudioManager.instance.bgmVolume * 100).ToString();
+            sfxInput.text = Mathf.RoundToInt(AudioManager.instance.sfxVolume * 100).ToString();
         }
 
-        bgmSlider.onValueChanged.AddListener(SetBgmVolume);
-        sfxSlider.onValueChanged.AddListener(SetSfxVolume);
+        // 슬라이더 > 숫자
+        bgmSlider.onValueChanged.AddListener(OnBgmSlider);
+        sfxSlider.onValueChanged.AddListener(OnSfxSlider);
+
+        // 숫자> 슬라이더
+        bgmInput.onEndEdit.AddListener(OnBgmInput);
+        sfxInput.onEndEdit.AddListener(OnSfxInput);
     }
 
-    void SetBgmVolume(float value)
+    void OnBgmSlider(float value)
     {
         AudioManager.instance.SetBgmVolume(value);
+        bgmInput.text = Mathf.RoundToInt(value * 100).ToString("00");
     }
 
-    void SetSfxVolume(float value)
+    void OnBgmInput(string text)
+    {
+        if (!int.TryParse(text, out int value))
+            value = 0;
+
+        value = Mathf.Clamp(value, 0, 100);
+
+        float volume = value / 100f;
+        bgmSlider.value = volume;
+        AudioManager.instance.SetBgmVolume(volume);
+
+        bgmInput.text = value.ToString("00");
+    }
+
+    void OnSfxSlider(float value)
     {
         AudioManager.instance.SetSfxVolume(value);
+        sfxInput.text = Mathf.RoundToInt(value * 100).ToString("00");
+    }
+
+    void OnSfxInput(string text)
+    {
+        if (!int.TryParse(text, out int value))
+            value = 0;
+
+        value = Mathf.Clamp(value, 0, 100);
+
+        float volume = value / 100f;
+        sfxSlider.value = volume;
+        AudioManager.instance.SetSfxVolume(volume);
+
+        sfxInput.text = value.ToString("00");
     }
 }
